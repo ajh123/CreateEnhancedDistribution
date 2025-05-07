f2cfa303fff594b5d496b16d0f580f850b98d762
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-05-07 10:04:56 +0100

    feat: extend ElectricalEnergyMeterBlock to support directional placement

--- src/main/java/me/ajh123/create_distribution/foundation/content/meter/ElectricalEnergyMeterBlock.java
+++ src/main/java/me/ajh123/create_distribution/foundation/content/meter/ElectricalEnergyMeterBlock.java
@@ -1,13 +1,44 @@
 package me.ajh123.create_distribution.foundation.content.meter;
 
+import com.mojang.serialization.MapCodec;
 import com.simibubi.create.foundation.block.IBE;
 import me.ajh123.create_distribution.foundation.ModBlocks;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import org.jetbrains.annotations.NotNull;
 
-public class ElectricalEnergyMeterBlock extends Block implements IBE<ElectricalEnergyMeterBlockEntity> {
+public class ElectricalEnergyMeterBlock extends HorizontalDirectionalBlock implements IBE<ElectricalEnergyMeterBlockEntity> {
+    public static final MapCodec<ElectricalEnergyMeterBlock> CODEC = simpleCodec(ElectricalEnergyMeterBlock::new);
+
     public ElectricalEnergyMeterBlock(Properties properties) {
         super(properties);
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        super.createBlockStateDefinition(pBuilder.add(FACING));
+    }
+
+    public BlockState getStateForPlacement(@NotNull BlockPlaceContext pContext) {
+        BlockState state = this.defaultBlockState();
+        Direction horizontalDirection = pContext.getHorizontalDirection();
+        Player player = pContext.getPlayer();
+        state = state.setValue(FACING, horizontalDirection.getOpposite());
+        if (player != null && player.isShiftKeyDown()) {
+            state = state.setValue(FACING, horizontalDirection);
+        }
+
+        return state;
+    }
+
+    @Override
+    protected @NotNull MapCodec<? extends HorizontalDirectionalBlock> codec() {
+        return CODEC;
     }
 
     @Override
